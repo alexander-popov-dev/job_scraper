21c1ddab295618c6a654dc0bb6529079113cbde9
Alexander Popov <alexander.popov.dev@gmail.com>
2026-08-28 18:41:09 +0300

    add: hedders and docstrings

--- sites/the_job/scraper.py
+++ sites/the_job/scraper.py
@@ -3,11 +3,19 @@ from core.dto import ResponseDTO
 
 
 class Scraper(BaseScraper):
-    """Fetches Python job listings from Djinni."""
+    """Fetches Python job listings from The Job."""
 
-    SITE_NAME = 'TheJob'
-    URL = 'https://thejob.tech/api/job?query&techs=Python&jobLocation&employmentType&page=1&company'
+    SITE_NAME = "TheJob"
+    URL = "https://thejob.tech/api/job?query=python&jobLocation=Remote&&page=1"
+    HEADERS = {
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/144.0.0.0 Safari/537.36"
+        )
+    }
 
     def _request(self) -> ResponseDTO:
-        """Send a GET request to the Djinni job listings page."""
-        return self._client.fetch(self.URL)
+        """Send a GET request to the The Job job listings page."""
+
+        return self._client.fetch(self.URL, headers=self.HEADERS)
